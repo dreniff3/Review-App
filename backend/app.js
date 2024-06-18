cd3493b36ce1145020f083e6e163416cffe89f46
Don Reniff <donaldreniff@gmail.com>
2024-06-18 12:40:57 -0700

    Import user router

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import { userRouter } from "./routes/user.js";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-});
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello World</h1>');
+// });
+
+app.use(userRouter);
 
 app.listen(8000, () => {
     console.log("Listening on port 8000...");
