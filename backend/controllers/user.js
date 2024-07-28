96207e1647ab6c3642b0e0312b1ace1adac4f2a7
Don Reniff <donaldreniff@gmail.com>
2024-07-28 16:23:22 -0700

    Add error handler for duplicate user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,10 +3,16 @@ import User from "../models/user.js";
 const createUser = async (req, res) => {
     const {name, email, password} = req.body;
 
+    // check if this user already exists in db
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+        return res.status(401).json({ error: "This email is already in use!"});
+    }
+
     const newUser = new User({name, email, password});
     await newUser.save();
 
-    res.json({ user: newUser });
+    res.status(201).json({ user: newUser });
 };
 
 export { createUser };
