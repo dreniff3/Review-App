c7d8bbc9f32f107721f6f635548962f5719f1b85
Don Reniff <donaldreniff@gmail.com>
2024-07-28 16:35:58 -0700

    Add express middleware tester function

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,20 @@ const app = express();
 app.use(express.json());
 app.use('/api/user', userRouter);
 
+// middleware tester:
+app.post("/sign-in", 
+    (req, res, next) => {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.json({ error: "email/password missing!"});
+        }
+        next();
+    },
+    (req, res) => {
+        res.send("<h1>Hello!</h1>");
+    }
+);
+
 app.listen(8000, () => {
     console.log("Listening on port 8000...");
 });
